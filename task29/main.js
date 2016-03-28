8fe7d387aef56c46ed3d3861528d1850a78a215f
Clect <clect@foxmail.com>
2016-03-28 19:42:20 +0800

    task 29
    提交

--- task29/main.js
+++ task29/main.js
@@ -3,15 +3,33 @@ var b = document.getElementById('tip');
 
 function xxx() {
     var msg = a.value;
-    if(msg.length == 0){
-        console.log(msg.length,msg);
+    if(GetLength(msg) == 0){
         document.getElementById('tip').innerHTML = '姓名不能为空';
         b.style.color = 'red';
         a.style.border = '2px solid red';
     }
-    if(msg.length >= 4 && msg.length <= 16){
+    else if(GetLength(msg) >= 4 && GetLength(msg) <= 16){
         document.getElementById('tip').innerHTML = '格式正确';
         b.style.color = 'lightgreen';
         a.style.border = '2px solid lightgreen';
     }
+    else {
+        document.getElementById('tip').innerHTML = '字符数应为4~16位';
+        b.style.color = 'red';
+        a.style.border = '2px solid red';
+    }
 }
+
+GetLength = function(str)   
+{  
+    var realLength = 0;  
+    for (var i = 0; i < str.length; i++)   
+    {  
+        charCode = str.charCodeAt(i);  
+        if (charCode >= 0 && charCode <= 128)   
+        realLength += 1;  
+        else   
+        realLength += 2;  
+    }  
+    return realLength; 
+}; 
